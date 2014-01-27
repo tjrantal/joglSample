a9fb444daa05069e147bbb83dcecb94b6ca5817b
Timo Rantalainen <tjrantal@gmail.com>
2014-01-28 10:13:44 +1100

    Modified the testi.sh to create a jar file and to add jarLib to classpath (copy native jarfiles to jarLib/ to make it work...)

--- testi.bat
+++ testi.bat
@@ -1,5 +1,6 @@
-javac -cp ".;gluegen.jar;jogl-all.jar" -source 1.6 -target 1.6 timo/test/KainTutorial.java
-java -cp ".;gluegen.jar;gluegen-natives-windows-amd64.jar;jogl-all.jar;jogl-all-natives-windows-amd64.jar" timo.test.KainTutorial
+javac -cp ".;jarLib/gluegen.jar;jarLib/jogl-all.jar" -source 1.6 -target 1.6 timo/test/KainTutorial.java
+jar cfm joglSample.jar manifest/MANIFEST.MF timo/test/*.class
+java -jar joglSample.jar
 
 
 ::java -Djava.library.path=lib -cp ".;gluegen.jar;gluegen-rt-natives-windows-amd64.jar;gluegen-rt-natives-windows-i586.jar;jogl-all.jar;jogl-all-natives-windows-amd64.jar" timo.test.KainTutorial
